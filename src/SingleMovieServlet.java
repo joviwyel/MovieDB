824420c8b9922b0630be60922c4497f38b7bf600
Jovi Wyel <joviwyel@Jovis-MacBook-Pro.local>
2021-04-08 16:10:35 -0700

    update for display stars name

--- src/SingleMovieServlet.java
+++ src/SingleMovieServlet.java
@@ -2,7 +2,9 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
-import javax.annotation.Resource;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,8 +26,15 @@ public class SingleMovieServlet extends HttpServlet {
     private static final long serialVersionUID = 3L;
 
     // Create a dataSource which registered in web.xml
-    @Resource(name = "jdbc/moviedb")
     private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
