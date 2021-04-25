b60e063c614a99af2a39fce4448c8210e4b5a9d6
Kai Li Tan <kylie.tn79@gmail.com>
2021-04-25 00:25:40 -0700

    Added payment page; bug: can't display total price yet

---
+++ WebContent/payment.js
@@ -0,0 +1,30 @@
+function getParameterByName(target) {
+    // Get request URL
+    let url = window.location.href;
+    // Encode target parameter name to url encoding
+    target = target.replace(/[\[\]]/g, "\\$&");
+
+    // Ues regular expression to find matched parameter value
+    let regex = new RegExp("[?&]" + target + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+
+    // Return the decoded parameter value
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
+function handleResult(resultData) {
+    let totalHTML = "";
+    totalHTML += "<th>" + resultData[0]["total"] + "</th>";
+}
+
+
+// Request data from shopping
+jQuery.ajax({
+    dataType: "json",  // Setting return data type
+    method: "POST",// Setting request method
+    url: "api/shopping cart", // Setting request url, which is mapped by StarsServlet in Stars.java
+    success: (resultData) => handleResult(resultData) // Setting callback function to handle data returned successfully by the SingleStarServlet
+});
