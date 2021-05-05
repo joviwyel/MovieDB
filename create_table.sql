9cb831779cb2dec50a99dbf3bc69c5e234e74ec3
Kai Li Tan <41200726+kaiiiliii@users.noreply.github.com>
2021-05-05 15:52:57 -0700

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -73,3 +73,10 @@ rating FLOAT NOT NULL,
 numVotes INT NOT NULL,
 FOREIGN KEY(movieId) REFERENCES movies(id)
 );
+
+CREATE TABLE employees(
+email varchar(50) primary key, 
+password varchar(20) not null, 
+fullname varchar(100)
+);
+
