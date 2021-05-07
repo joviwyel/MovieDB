b4183f88da31457f0f10e88d448b2d3059dc6b5e
Kai Li Tan <41200726+kaiiiliii@users.noreply.github.com>
2021-05-07 13:49:07 -0700

    Add insert movie stored procedure

--- stored-procedure.sql
+++ stored-procedure.sql
@@ -1,15 +1,23 @@
--- DASHBOARD: generate new star id
+-- DASHBOARD: generate new star and movie id
 DELIMITER $$
-CREATE PROCEDURE get_id(IN last_id VARCHAR(10))
+CREATE PROCEDURE get_id()
 BEGIN
-    SELECT CONCAT(SUBSTR(last_id, 1, 2), (CAST(SUBSTR((last_id), 3) AS UNSIGNED)+1)) AS id;
+    SELECT CONCAT(SUBSTR((SELECT MAX(id) FROM stars), 1, 2), (CAST(SUBSTR((SELECT MAX(id) FROM stars), 3) AS UNSIGNED)+1)) AS id;
 END $$
 DELIMITER ;
 
 -- DASHBOARD: generate new genre id 
 DELIMITER $$
-CREATE PROCEDURE get_genre_id(IN last_id VARCHAR(10))
+CREATE PROCEDURE get_genre_id()
 BEGIN
-    SELECT CONCAT(CAST(SUBSTR((last_id), 1) AS UNSIGNED)+1) AS id;
+    SELECT CONCAT(CAST(SUBSTR((SELECT MAX(id) FROM genres), 1) AS UNSIGNED)+1) AS id;
+END $$
+DELIMITER ;
+
+-- DASHBOARD: insert new movie
+DELIMITER $$
+CREATE PROCEDURE insert_movie(IN movie_id VARCHAR(10), title VARCHAR(100),  year int, director VARCHAR(100))
+BEGIN
+    INSERT movies VALUES (movie_id, title, year, director);
 END $$
 DELIMITER ;
