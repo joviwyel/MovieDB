419ae19903e0c5951cefefe68808e0ca1d4ecb92
Kai Li Tan <41200726+kaiiiliii@users.noreply.github.com>
2021-05-07 13:00:06 -0700

    Add genre id stored procedure

--- stored-procedure.sql
+++ stored-procedure.sql
@@ -1,7 +1,15 @@
--- DASHBOARD: get_id
+-- DASHBOARD: generate new star id
 DELIMITER $$
 CREATE PROCEDURE get_id(IN last_id VARCHAR(10))
 BEGIN
     SELECT CONCAT(SUBSTR(last_id, 1, 2), (CAST(SUBSTR((last_id), 3) AS UNSIGNED)+1)) AS id;
+END $$
+DELIMITER ;
+
+-- DASHBOARD: generate new genre id 
+DELIMITER $$
+CREATE PROCEDURE get_genre_id(IN last_id VARCHAR(10))
+BEGIN
+    SELECT CONCAT(CAST(SUBSTR((last_id), 1) AS UNSIGNED)+1) AS id;
 END $$
 DELIMITER ;
