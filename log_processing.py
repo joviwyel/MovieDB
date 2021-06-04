d2af594cf10171f3ca405ff68aaac59650447d9d
joviwyel <58997743+joviwyel@users.noreply.github.com>
2021-06-04 15:35:23 -0700

    Update log_processing.py

--- log_processing.py
+++ log_processing.py
@@ -1,13 +1,22 @@
 import sys
 
+
+def nonblank_lines(f):
+	for l in f:
+		line = l.rstrip()
+		if line:
+			yield line
+
+
 if __name__ == "__main__":
-    TSs = []
-    TJs = []
-    with open(sys.argv[1]) as f:
-        lines = f.readlines()
-        for line in lines:
-            temp = line.split(',')
-            TSs.append(int(temp[0].split(':')[1]))
-            TJs.append(int(temp[1].split(':')[1].strip("\n")))
-    print("AVE TS : ", (sum(TSs)/len(TSs))/(10**6), 
+	TSs = []
+	TJs = []
+	with open(sys.argv[1]) as f:
+		for line in nonblank_lines(f):
+			temp = line.split(',')
+			TSs.append(int(temp[0].split(':')[1]))
+			TJs.append(int(temp[1].split(':')[1].strip("\n")))
+	print("AVE TS : ", (sum(TSs)/len(TSs))/(10**6), 
     	  "AVE TJ : ", (sum(TJs)/len(TJs))/(10**6))
+    	  
+	f.close()
